ca002d9e4746a1deffade24f256aa6254ad4c196
kaustav24 <61621407+kaustav24@users.noreply.github.com>
2020-03-01 03:52:39 +0530

    Add files via upload

---
+++ gsm_0.ino
@@ -0,0 +1,32 @@
+#include<SoftwareSerial.h>
+SoftwareSerial gsm(2,3);
+void setup()
+{
+  delay(2000);
+  Serial.begin(9600);
+  gsm.begin(9600);
+  gsm.println("AT+CMGF=1");
+  delay(1000);
+  gsm.println("AT+CMGS=\"+918957679338\"\r");
+  delay(1000);
+  gsm.println("hello");
+  delay(500);
+  gsm.println((char)26);
+  delay(2000);
+    
+    
+  gsm.println("AT+CMGF=1");
+  delay(1000);
+  gsm.println("AT+CMGS=\"+918948041190\"\r");
+  delay(1000);
+  gsm.println("hello");
+  delay(500);
+  gsm.println((char)26);
+  delay(2000);
+  
+  Serial.println("send");
+}
+void loop()
+{
+  
+}
